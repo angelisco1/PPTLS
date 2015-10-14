1bbe845e10b3658bd7fa77c806755700212e83d4
Angel Villalba <angelvil92@gmail.com>
2015-10-14 16:22:52 +0200

    Code refactorized

--- client/templates/vsMachine/machine.js
+++ client/templates/vsMachine/machine.js
@@ -1,40 +1,8 @@
 Template.vsmachine.events({
-	'click .rock': function (event) {
-		Session.set('resultP', "Rock");
-		Meteor.call("play", 1, function(err, res){
+	'click .btn-option': function(event){
+		var id = event.currentTarget.attributes.id.value;
+		Meteor.call("play", id, function(err, res){
 			if(!err){
-				Session.set('result', res);
-			}
-		});
-	},
-	'click .paper': function (event) {
-		Session.set('resultP', "Paper");
-		Meteor.call('play', 2, function(err,res) {
-			if (!err){
-				Session.set('result', res);
-			}
-		});
-	},
-	'click .scissors': function (event) {
-		Session.set('resultP', "Scissors");
-		Meteor.call('play', 3, function(err,res) {
-			if (!err){
-				Session.set('result', res);
-			}
-		});
-	},
-	'click .lizard': function (event) {
-		Session.set('resultP', "Lizard");
-		Meteor.call('play', 4, function(err,res) {
-			if (!err){
-				Session.set('result', res);
-			}
-		});
-	},
-	'click .spock': function (event) {
-		Session.set('resultP', "Spock");
-		Meteor.call('play', 5, function(err,res) {
-			if (!err){
 				Session.set('result', res);
 			}
 		});
@@ -50,5 +18,14 @@ Template.vsmachine.helpers({
 	},
 	resultMachine: function(){
 		return Session.get('result').p2;
+	},
+	getDataVsMachineGanadas: function(){
+		return Games.findOne({userId: Meteor.userId()}).mode.vsmachine.ganadas || 0;
+	},
+	getDataVsMachineEmpatadas: function(){
+		return Games.findOne({userId: Meteor.userId()}).mode.vsmachine.empatadas || 0;
+	},
+	getDataVsMachinePerdidas: function(){
+		return Games.findOne({userId: Meteor.userId()}).mode.vsmachine.perdidas || 0;
 	}
 });
--- server/methods.js
+++ server/methods.js
@@ -1,73 +1,103 @@
 var generateOption = function(){
-  return Math.floor(Math.random() * 5) + 1;
+  return Math.floor(Math.random() * 5);
 }
 
 var getOption = function(opt){
   switch(opt){
-    case 1: 
-      return "/images/rock.png";
-      break;
-    case 2: 
-      return "/images/paper.png";
-      break;
-    case 3: 
+    case 0: 
       return "/images/scissors.png";
       break;
-    case 4: 
+    case 1: 
+      return "/images/paper.png";
+      break;
+    case 2: 
+      return "/images/rock.png";
+      break;
+    case 3: 
       return "/images/lizard.png";
       break;
-    case 5: 
+    case 4: 
       return "/images/spock.png";
       break;
   }
 }
 
 Meteor.methods({
-    play: function(option1){
+    play: function(opt){
+      var option1 = parseInt(opt);
     	var option2 = generateOption();
-      if(option1 == option2){
-      	return {p1: getOption(option1), p2: getOption(option2), res: "/images/draw.png"};
-      }else{
-      	switch(option1){
-      		case 1:
-      			if(option2 == 3 || option2 == 4){
-      				return {p1: getOption(option1), p2: getOption(option2), res: "/images/win.png"};
-      			}else{
-      				return {p1: getOption(option1), p2: getOption(option2), res: "/images/lose.png"};
-      			}
-      			break;
-      		case 2:
-      			if(option2 == 1 || option2 == 5){
-              return {p1: getOption(option1), p2: getOption(option2), res: "/images/win.png"};
-      			}else{
-              return {p1: getOption(option1), p2: getOption(option2), res: "/images/lose.png"};
-      			}
-      			break;
-      		case 3:
-      			if(option2 == 2 || option2 == 4){
-              return {p1: getOption(option1), p2: getOption(option2), res: "/images/win.png"};
-      			}else{
-              return {p1: getOption(option1), p2: getOption(option2), res: "/images/lose.png"};
-      			}
-      			break;
-      		case 4:
-      			if(option2 == 2 || option2 == 5){
-              return {p1: getOption(option1), p2: getOption(option2), res: "/images/win.png"};
-      			}else{
-              return {p1: getOption(option1), p2: getOption(option2), res: "/images/lose.png"};
-      			}
-      			break;
-      		case 5:
-      			if(option2 == 1 || option2 == 3){
-              return {p1: getOption(option1), p2: getOption(option2), res: "/images/win.png"};
-      			}else{
-              return {p1: getOption(option1), p2: getOption(option2), res: "/images/lose.png"};
-      			}
-      			break;
 
-      	}
+      if(option1 == option2){
+        return {p1: getOption(option1), p2: getOption(option2), res: "/images/draw.png"};
+      }else{
+        if((option1+1)%5 == option2 || (option1+3)%5 == option2){
+          return {p1: getOption(option1), p2: getOption(option2), res: "/images/win.png"};
+        }else{
+          return {p1: getOption(option1), p2: getOption(option2), res: "/images/lose.png"};
+        }
       }
 
+
+
+
+      // if(option1 == option2){
+      // 	return {p1: getOption(option1), p2: getOption(option2), res: "/images/draw.png"};
+      // }else{
+      // 	switch(option1){
+      // 		case 1:
+      // 			if(option2 == 3 || option2 == 4){
+      // 				return {p1: getOption(option1), p2: getOption(option2), res: "/images/win.png"};
+      // 			}else{
+      // 				return {p1: getOption(option1), p2: getOption(option2), res: "/images/lose.png"};
+      // 			}
+      // 			break;
+      // 		case 2:
+      // 			if(option2 == 1 || option2 == 5){
+      //         return {p1: getOption(option1), p2: getOption(option2), res: "/images/win.png"};
+      // 			}else{
+      //         return {p1: getOption(option1), p2: getOption(option2), res: "/images/lose.png"};
+      // 			}
+      // 			break;
+      // 		case 3:
+      // 			if(option2 == 2 || option2 == 4){
+      //         return {p1: getOption(option1), p2: getOption(option2), res: "/images/win.png"};
+      // 			}else{
+      //         return {p1: getOption(option1), p2: getOption(option2), res: "/images/lose.png"};
+      // 			}
+      // 			break;
+      // 		case 4:
+      // 			if(option2 == 2 || option2 == 5){
+      //         return {p1: getOption(option1), p2: getOption(option2), res: "/images/win.png"};
+      // 			}else{
+      //         return {p1: getOption(option1), p2: getOption(option2), res: "/images/lose.png"};
+      // 			}
+      // 			break;
+      // 		case 5:
+      // 			if(option2 == 1 || option2 == 3){
+      //         return {p1: getOption(option1), p2: getOption(option2), res: "/images/win.png"};
+      // 			}else{
+      //         return {p1: getOption(option1), p2: getOption(option2), res: "/images/lose.png"};
+      // 			}
+      // 			break;
+
+      // 	}
+      // }
+
+    },
+    updateDataVsMachine: function(ganadas, perdidas, empatadas){
+      if(!Meteor.user()){
+        throw new Meteor.Error('not-authorized');
+      }
+
+      Games.update(Meteor.userId(), {
+        $set: {
+          vsmachine: {
+            ganadas: ganadas,
+            perdidas: perdidas,
+            empatadas: empatadas
+          }
+        }
+      });
     }
 
   });
